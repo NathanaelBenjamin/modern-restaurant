fb1fc2310db1d02d36e48d57a2200121eb6a1c26
ibenacho Elvis <ibenachoelvis49@gmail.com>
2022-04-24 01:13:31 +0100

    up

--- modern-restuarant/src/components/pages/About.js
+++ modern-restuarant/src/components/pages/About.js
@@ -1,11 +1,21 @@
+import React from 'react'
+import './style.css';
 
-
-const About = () =>{
-    return(
-        <div>
-
-        </div>
-    )
+function About() {
+  return (
+    <div className='main-container'>
+         <div className='nav-container'>
+        <ul>
+            <li><a href='#'>Home</a></li>
+            <li><a href='#'>Menu</a></li>
+            <li><a href='#'>Our Story</a></li>
+            <li><a href='#'>Reviews</a></li>
+            <li><a href='#'>Contact</a></li>
+        </ul>
+    </div>
+    </div>
+   
+  )
 }
 
 export default About
